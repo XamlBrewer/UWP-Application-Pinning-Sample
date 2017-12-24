70a5f1e757541f245c735348f1974879abb373db
Diederik Krols <diederik_krols@hotmail.com>
2017-12-24 11:41:03 +0100

    Pin to TaskBar and Pint to StartMenu.

---
+++ XamlBrewer.Uwp.ApplicationPinningSample/Services/Pinning/Pinning.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation.Metadata;
+using Windows.UI.Shell;
+using Windows.UI.StartScreen;
+
+namespace Mvvm.Services
+{
+    public class Pinning
+    {
+        public async static Task<bool?> IsPinnedToTaskBar()
+        {
+            if (ApiInformation.IsTypePresent("Windows.UI.Shell.TaskbarManager"))
+            {
+                return await TaskbarManager.GetDefault().IsCurrentAppPinnedAsync();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static async Task<bool?> RequestPinToTaskBar()
+        {
+            if (ApiInformation.IsTypePresent("Windows.UI.Shell.TaskbarManager"))
+            {
+                return await TaskbarManager.GetDefault().RequestPinCurrentAppAsync();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static async Task<bool?> IsPinnedToStartMenu()
+        {
+            if (ApiInformation.IsTypePresent("Windows.UI.StartScreen.StartScreenManager"))
+            {
+                AppListEntry entry = (await Package.Current.GetAppListEntriesAsync())[0];
+                return await StartScreenManager.GetDefault().ContainsAppListEntryAsync(entry);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static async Task<bool?> RequestPinToStartMenu()
+        {
+            if (ApiInformation.IsTypePresent("Windows.UI.StartScreen.StartScreenManager"))
+            {
+                AppListEntry entry = (await Package.Current.GetAppListEntriesAsync())[0];
+                return await StartScreenManager.GetDefault().RequestAddAppListEntryAsync(entry);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async static Task<bool> HasSecondaryTiles()
+        {
+            var tiles = await SecondaryTile.FindAllAsync();
+            return tiles.Count > 0;
+        }
+    }
+}
--- XamlBrewer.Uwp.ApplicationPinningSample/Views/MainPage.xaml.cs
+++ XamlBrewer.Uwp.ApplicationPinningSample/Views/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Mvvm;
+using Mvvm.Services;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml.Controls;
 
 namespace XamlBrewer.Uwp.ApplicationPinningSample
 {
@@ -7,6 +11,79 @@ namespace XamlBrewer.Uwp.ApplicationPinningSample
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        public string IsPinnedToTaskBar()
+        {
+            var isPinned = Task.Run<bool?>(() => Pinning.IsPinnedToTaskBar()).Result;
+
+            switch (isPinned)
+            {
+                case true:
+                    return "This app is pinned to the task bar.";
+                case false:
+                    return "This app is not pinned to the task bar.";
+                default:
+                    return "I cannot determine whether this app is pinned to the task bar.";
+            }
+        }
+
+        public string IsPinnedToStartMenu()
+        {
+            var isPinned = Task.Run<bool?>(() => Pinning.IsPinnedToStartMenu()).Result;
+
+            switch (isPinned)
+            {
+                case true:
+                    return "This app is pinned to the start menu.";
+                case false:
+                    return "This app is not pinned to the start menu.";
+                default:
+                    return "I cannot determine whether this app is pinned to the start menu.";
+            }
+        }
+
+        public string HasSecondaryTiles()
+        {
+            var hasSecondaryTiles = Task.Run<bool>(() => Pinning.HasSecondaryTiles()).Result;
+            if (hasSecondaryTiles)
+            {
+                return "This app has secondary tiles.";
+            }
+
+            return "This app has no secondary tiles.";
+        }
+
+        public ICommand PinToTaskBarCommand
+        {
+                get { return new DelegateCommand(PinToTaskBar_Executed, PinToTaskBar_CanExecute); }
+        }
+
+        public ICommand PinToStartMenuCommand
+        {
+            get { return new DelegateCommand(PinToStartMenu_Executed, PinToStartMenu_CanExecute); }
+        }
+
+        private bool PinToTaskBar_CanExecute()
+        {
+            var isPinned = Task.Run<bool?>(() => Pinning.IsPinnedToTaskBar()).Result;
+            return !isPinned ?? false;
+        }
+
+        private async void PinToTaskBar_Executed()
+        {
+            await Pinning.RequestPinToTaskBar();
+        }
+
+        private bool PinToStartMenu_CanExecute()
+        {
+            var isPinned = Task.Run<bool?>(() => Pinning.IsPinnedToStartMenu()).Result;
+            return !isPinned ?? false;
+        }
+
+        private async void PinToStartMenu_Executed()
+        {
+            await Pinning.RequestPinToStartMenu();
         }
     }
 }
